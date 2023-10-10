9650f0d2dcf1ac6084bd506c4626015dd70c64f6
Strom Norcross <ywu6av@virginia.edu>
2023-10-09 22:27:35 -0400

    reverting

--- roomFinder/settings.py
+++ roomFinder/settings.py
@@ -109,11 +109,11 @@ WSGI_APPLICATION = "roomFinder.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(),
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
+    # "default": dj_database_url.config(),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
@@ -169,6 +169,6 @@ try:
 except ImportError:
     found = False
 
-# if 'DATABASE_URL' in os.environ:
-#     import dj_database_url
-#     DATABASES = {'default': }
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
