96ce94c7975c3cc8e0ee5e585a288fd69803e93e
emilybliu <vyj9jn@virginia.edu>
2023-10-17 19:22:52 -0400

    set up basic models

--- roomFinder_app/models.py
+++ roomFinder_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    room_name = models.CharField(max_length=50)
+    room_location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.room_name
+
+class Reservation(models.Model):
+    room = models.ManyToManyField(Room, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User, on_delete=models.CASCADE)
+
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    user_type = models.TextChoices("user_type", "USER ADMIN")
