d274ad90b30a4599a860d597003a2cf186929b76
emilybliu <vyj9jn@virginia.edu>
2023-10-17 20:21:16 -0400

    updating models

--- roomFinder_app/models.py
+++ roomFinder_app/models.py
@@ -4,17 +4,27 @@ from django.db import models
 class Room(models.Model):
     room_name = models.CharField(max_length=50)
     room_location = models.CharField(max_length=100)
+    capacity = models.PositiveIntegerField()
 
     def __str__(self):
         return self.room_name
 
-class Reservation(models.Model):
-    room = models.ManyToManyField(Room, on_delete=models.CASCADE)
-    user = models.ManyToManyField(User, on_delete=models.CASCADE)
 
+class Reservation(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user + " - " + self.room
 
 
 class User(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     user_type = models.TextChoices("user_type", "USER ADMIN")
+
+    def __str__(self):
+        return self.name + " - " + self.email
