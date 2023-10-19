f2eb4ae17187a9f91f3a78b797baf36d8e6e0f20
Celestine Nguyen <haw3wk@virginia.edu>
2023-10-19 13:34:53 -0400

    Added was_created_recently to Reservation

--- roomFinder_app/models.py
+++ roomFinder_app/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Room(models.Model):
@@ -25,6 +29,10 @@ class Reservation(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def was_created_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.created_at <= now
 
     def __str__(self):
         return self.user + " - " + self.room
