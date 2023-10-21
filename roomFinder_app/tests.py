dda64218b40a9ba2b8e2ef5a889099ca1d18ae07
Celestine Nguyen <haw3wk@virginia.edu>
2023-10-21 17:17:08 -0400

    wrote and passed test_was_created_recently_with_old_reservation

--- roomFinder_app/tests.py
+++ roomFinder_app/tests.py
@@ -35,3 +35,17 @@ class ReservationModelTests(TestCase):
         future_res = Reservation(room=room, user=user, start_time=start, end_time=end, created_at=time)
         self.assertIs(future_res.was_created_recently(), False)
     
+    def test_was_created_recently_with_old_reservation(self):
+        """
+        was_created_recently() returns False for reservations whose created_at
+        is older than 1 day.
+        """
+        room = create_room("Rice 130", "Rice Hall", 100)
+        user = create_user("Bob", "email@email.com")
+
+        start = timezone.now()
+        end = timezone.now() + datetime.timedelta(hours=2)
+        
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_res = Reservation(room=room, user=user, start_time=start, end_time=end, created_at=time)
+        self.assertIs(old_res.was_created_recently(), False)
