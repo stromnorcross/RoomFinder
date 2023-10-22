612cf0ad69e7b8d1eb462af1848b636603a9f543
emilybliu <vyj9jn@virginia.edu>
2023-10-22 13:44:55 -0400

    created views to see list of rooms and it's information when clicked

--- roomFinder_app/views.py
+++ roomFinder_app/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.utils import timezone
+from django.views import generic
 
-# Create your views here.
+from .models import Room, User, Reservation
+
+
+
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "room_list"
+
+    def get_queryset(self):
+        return Room.objects.all()
+    
+class RoomDetailView(generic.DetailView):
+    model = Room
+    template_name = "room_detail.html"
+    
+    def get_queryset(self):
+        return Room.objects.all()
+
+"""
+TODO
+class ReservationDetailView(generic.DetailView):
+    model = Reservation
+    template_name = "reservation_detail.html"
+
+    def get_queryset(self):
+        return Reservation.object.all()
+"""
+
+"""
+TODO
+class ReservationDetailView(generic.DetailView):
+    model = Reservation
+    template_name = "reservation_detail.html"
+
+    def get_queryset(self):
+        return Reservation.object.all()
+"""
+
