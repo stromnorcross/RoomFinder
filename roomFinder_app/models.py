97190b916a060ab31a064fed0b83354c4f0b64cb
Strom Norcross <ywu6av@virginia.edu>
2023-11-03 12:58:57 -0400

    admin and user differentiation

--- roomFinder_app/models.py
+++ roomFinder_app/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
 #from django.contrib.postgres.fields import ArrayField
 
 
