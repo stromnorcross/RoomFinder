e9c0b5eb4c603856addcf8d4fc58fde269521b13
Strom Norcross <ywu6av@virginia.edu>
2023-11-08 11:51:39 -0500

    making migrations on heroku

--- roomFinder_app/views.py
+++ roomFinder_app/views.py
@@ -17,32 +17,32 @@ import requests
 from datetime import timedelta,datetime
 import re
 
-# def dummy_user():
-#     try:
-#         return User.objects.get(username='class time')
-#     except ObjectDoesNotExist:
-#         print("Create a Dummy User")
+def dummy_user():
+    try:
+        return User.objects.get(username='class time')
+    except ObjectDoesNotExist:
+        print("Create a Dummy User")
 
-# def import_data():
-#     try:
-#         user = dummy_user()
-#         df = pd.read_csv("roomFinder_app/class_res.csv")
-#         for index, row in df.iterrows():
-#             if not Room.objects.filter(room_name=row['Room'], building=row['Building']).exists():
-#                 room = Room(room_name=row['Room'],building=row['Building'])
-#                 room.save()
-#             room = Room.objects.get(room_name=row['Room'],building=row['Building'])
-#             reservation = Reservation(title='Class',room=room,user=user,start_time=datetime.strptime(str(row['Start_time']), '%H.%M'),
-#                                       end_time=datetime.strptime(row['End_time'], '%H:%M:%S'),day=row['Days'])
-#             reservation.save()
-#     except IntegrityError:
-#         print("Create a Dummy User")
+def import_data():
+    try:
+        user = dummy_user()
+        df = pd.read_csv("roomFinder_app/class_res.csv")
+        for index, row in df.iterrows():
+            if not Room.objects.filter(room_name=row['Room'], building=row['Building']).exists():
+                room = Room(room_name=row['Room'],building=row['Building'])
+                room.save()
+            room = Room.objects.get(room_name=row['Room'],building=row['Building'])
+            reservation = Reservation(title='Class',room=room,user=user,start_time=datetime.strptime(str(row['Start_time']), '%H.%M'),
+                                      end_time=datetime.strptime(row['End_time'], '%H:%M:%S'),day=row['Days'])
+            reservation.save()
+    except IntegrityError:
+        print("Create a Dummy User")
 
 class IndexView(generic.ListView):
     template_name = "index.html"
     context_object_name = "room_list"
-    # if Reservation.objects.count() < 6756:
-    #     import_data()
+    if Reservation.objects.count() < 6756:
+        import_data()
 
     def get_queryset(self):
         return Room.objects.all()
