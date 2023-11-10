916af360a1feda9c10ca11ecbce4431cf5c62db1
Strom Norcross <ywu6av@virginia.edu>
2023-11-09 21:44:08 -0500

    creating view for room details

--- roomFinder_app/urls.py
+++ roomFinder_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("map/", TemplateView.as_view(template_name="map.html"), name="map"),
     path('room_list/<str:building_value>/', views.room_list, name='room_list'),
-    path("rooms/<int:pk>/details/", views.RoomDetailView.as_view(), name="room_detail"),
+    path("room_list/<int:pk>/details/", views.RoomDetailView.as_view(), name="room_detail"),
     path("reservations/<int:pk>/details/", views.ReservationDetailView.as_view(), name="reservation_detail"),
     path("reservations/", views.ReservationListView.as_view(), name="reservation_list"),
     path("create/", views.CreateResView.as_view(), name="create_reservation"),
