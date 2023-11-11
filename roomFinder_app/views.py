202f3551f250ba383d6d72913147f3e9bb2aaf73
Celestine Nguyen <haw3wk@virginia.edu>
2023-11-10 23:54:55 -0500

    attempt to resolve merge conflicts

--- roomFinder_app/views.py
+++ roomFinder_app/views.py
@@ -7,6 +7,39 @@ from django.contrib.auth.models import User
 from .models import Room, Reservation
 from .forms import ReservationForm
 from django.views.generic import CreateView
+from django.core.exceptions import ObjectDoesNotExist
+# from separate import room_generate
+import datetime
+import json
+import pandas as pd
+import requests
+from datetime import timedelta,datetime
+import re
+
+def dummy_user():
+    try:
+        return User.objects.get(username='class time')
+    except ObjectDoesNotExist:
+        print("Create a Dummy User")
+
+def import_data():
+    try:
+        user = dummy_user()
+        df = pd.read_csv("roomFinder_app/class_res.csv")
+        for index, row in df.iterrows():
+            if not Room.objects.filter(room_name=row['Room'], building=row['Building']).exists():
+                room = Room(room_name=row['Room'],building=row['Building'])
+                room.save()
+            room = Room.objects.get(room_name=row['Room'],building=row['Building'])
+            reservation = Reservation(title='Class',room=room,user=user,start_time=datetime.strptime(str(row['Start_time']), '%H.%M'),
+                                      end_time=datetime.strptime(row['End_time'], '%H:%M:%S'),day=row['Days'])
+            reservation.save()
+    except IntegrityError:
+        print("Create a Dummy User")
+
+def room_list(request, building_value):
+    objects = Room.objects.filter(building=building_value)
+    return render(request, 'room_list.html', {'objects': objects, 'building_value': building_value})
 
 
 class IndexView(generic.ListView):
@@ -14,7 +47,13 @@ class IndexView(generic.ListView):
     context_object_name = "room_list"
 
     def get_queryset(self):
-        return Room.objects.all()
+        seen = set()
+        uniqueBuildings = []
+        for room in Room.objects.all().order_by('building'):
+            if room.building not in seen:
+                seen.add(room.building)
+                uniqueBuildings.append(room.building)
+        return uniqueBuildings
 
 
 class RoomDetailView(generic.DetailView):
@@ -44,25 +83,28 @@ class RoomListView(generic.ListView):
 def make_reservation(request):
     if request.method == "POST":
         # change room_id for POST to be expected request
+        building = request.POST['building']
         room_name = request.POST['room_name']
-        room = Room.objects.all().get(room_name=room_name)
-        # for reservation in Reservation.objects.all().filter(room=room):
-        # # only allow booking if the requested start time is after the reservation end time
-        # # or requested end time is before reservation start time, need to check hotel reservation logic for if-elif
-        #     if str(reservation.start_time) > request.POST['start_time'] and str(reservation.start_time) > request.POST['end_time']:
-        #     # pass is a keyword that does nothing, kinda like break but instead it just lets the loop keep running to check
-        #     # the requested start and end times with other reservations
-        #         pass
-        #     elif str(reservation.end_time) < request.POST['start_time'] and str(reservation.end_time) < request.POST['end_time']:
-        #         pass
-        #     else:
-        #         #messages.warning(request, "Invalid Booking Time")
-        #         #return redirect("homepage")
+        room = Room.objects.all().get(room_name=room_name, building=building)
+        input_start_time = datetime.time(datetime.strptime(request.POST['start_time'], '%H:%M'))
+        input_end_time = datetime.time(datetime.strptime(request.POST['end_time'], '%H:%M'))
+        for reservation in Reservation.objects.all().filter(room=room):
+            if not(reservation.day == request.POST['day']):
+                pass
+            else:
+                if reservation.start_time > input_start_time and reservation.start_time >= input_end_time > input_start_time:
+                    pass
+                elif reservation.end_time <= input_start_time < input_end_time and reservation.end_time < input_end_time:
+                    pass
+                else:
+                    messages.warning(request, "Invalid Booking Time: A Reservation Exists For This Time")
+                    return HttpResponseRedirect(reverse('roomFinder_app:create_reservation'))
+                    # placeholder for now need to decide what to do if invalig, redirect to page or keep on page and have them re-enter
+                    # return redirect("create_reservation")
 
         current_user = request.user
-        #booking_id = str(room_id) + str(datetime.datetime.now())
         reservation = Reservation()
-        room_object = Room.objects.all().get(room_name=room_name)
+        room_object = Room.objects.all().get(room_name=room_name, building=building)
         print(room_object)
         user_object = User.objects.all().get(username=current_user)
         print(user_object)
@@ -71,11 +113,13 @@ def make_reservation(request):
         reservation.title = request.POST['title']
         reservation.start_time = request.POST['start_time']
         reservation.end_time = request.POST['end_time']
+        reservation.day = request.POST['day']
 
         reservation.save()
-        print(reservation)
-
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+        #print(reservation)
+        reservation_pk = reservation.pk
+        reservation_detail_url = reverse('roomFinder_app:reservation_detail', args=[reservation_pk])
+        return HttpResponseRedirect(reservation_detail_url)
     else:
         return HttpResponse('Access Denied')
 
