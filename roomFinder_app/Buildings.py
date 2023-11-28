e4d762e67bf4180fe9a2ce3f104e2f93447f94bc
Ahyush Kaul <ahyushkaul1@gmail.com>
2023-11-28 11:24:04 -0500

    update to fix more building names

--- roomFinder_app/Buildings.py
+++ roomFinder_app/Buildings.py
@@ -6,11 +6,16 @@ def buildings():
 
     df['Building'] = df['Building'].replace({'Engr': 'Engineering'}, regex=True)
     df['Building'] = df['Building'].str.replace('Bldg', '', regex=False)
+    df['Building'] = df['Building'].str.replace(' Room', '', regex=False)
+    df['Building'] = df['Building'].str.replace('The ', '', regex=False)
+    df['Building'] = df['Building'].str.replace(' Rm', '', regex=False)
     df['Building'] = df['Building'].str.replace('  ', ' ', regex=True)
     df.loc[df['Building'].str.contains('Chemistry', case=False), 'Building'] = 'Chemistry'
     df.loc[df['Building'].str.contains('Darden', case=False), 'Building'] = 'Darden'
     df['Building'] = df['Building'].str.replace(r' (B|G)$', '', regex=True)
     df['Room'] = df['Room'].apply(lambda x: f"{x:03d}")
+    df = df[~df['Building'].str.contains('CT', case=False)]
+    df = df[~df['Building'].str.contains('UVA', case=False)]
     df["Building"] = df["Building"].str.strip()
 
     df.to_csv("class_res.csv")
